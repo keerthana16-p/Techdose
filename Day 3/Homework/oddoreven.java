ccc8b61ac8ae066dfecd8ed5e9debc3d89732b08
keerthana16-p <108286359+keerthana16-p@users.noreply.github.com>
2022-09-06 20:24:49 +0530

    Create oddoreven.java

---
+++ Day 3/Homework/oddoreven.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{   
+	public static void main(String[] args) {
+	 Scanner scanner = new Scanner(System.in);
+	 int n = scanner.nextInt();
+	 if((n&1)==1){
+	     System.out.println("Odd");
+	 }
+	 else{
+	     System.out.println("Even");
+	 }
+	}
+}
