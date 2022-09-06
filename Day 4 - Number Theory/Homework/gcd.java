7edbb402c636cbef6d388d4c2dc32d8be18be6fd
keerthana16-p <108286359+keerthana16-p@users.noreply.github.com>
2022-09-06 22:13:11 +0530

    Create gcd.java

---
+++ Day 4 - Number Theory/Homework/gcd.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main
+{   
+    public static int gcd(int a,int b){
+       if(a==0){
+           return b;
+       }
+       return gcd(b%a,a);
+    }
+	public static void main(String[] args) {
+	 Scanner scanner = new Scanner(System.in);
+	 int a= scanner.nextInt();
+	 int b= scanner.nextInt();
+	 System.out.println(gcd(a,b));
+	}
+}
