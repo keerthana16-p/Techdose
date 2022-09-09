ada686ea4c6f6c9ffd333e1877f5a128fc14bed0
keerthana16-p <108286359+keerthana16-p@users.noreply.github.com>
2022-09-09 14:32:33 +0530

    Update InsertionSort.java

--- Sorting Algorithms/InsertionSort.java
+++ Sorting Algorithms/InsertionSort.java
@@ -1,1 +1,33 @@
+import java.util.Scanner;
+public class Main
+{   
+    public static void Swap(int[] arr,int a,int b){
+        int temp=arr[a];
+        arr[a]=arr[b];
+        arr[b]=temp;
+    }
+    public static void InsertionSort(int[] arr,int n){
+        for(int i=1;i<n;i++){
+            for(int j=i;j>0;j--){
+                if(arr[j]>arr[j-1]){
+                    break;
+                }
+                Swap(arr,j,j-1);
+            }
+        }
+    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int n = scanner.nextInt();
+		int[] arr = new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=scanner.nextInt();
+		}
+		InsertionSort(arr,n);
+		for(int i=0;i<n;i++){
+		    System.out.print(arr[i]+" ");
+		}
+	}
+}
+
 
