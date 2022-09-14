2c3f7ae63da41e75a21d02dde64cdfea434f9235
keerthana16-p <108286359+keerthana16-p@users.noreply.github.com>
2022-09-14 19:29:54 +0530

    Create 20.2235.AddTwoIntegers.java

---
+++ Beginner's Assignment/20.2235.AddTwoIntegers.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main
+{   
+    public static int add(int a,int b){
+        return a+b;
+    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int a = scanner.nextInt();
+		int b = scanner.nextInt();
+		System.out.println(add(a,b));
+	}
+}
