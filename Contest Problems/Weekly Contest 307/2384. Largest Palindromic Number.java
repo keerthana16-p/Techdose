77b7ebeacb1f3fbae37cf4c1da64800fa21d6146
keerthana16-p <108286359+keerthana16-p@users.noreply.github.com>
2022-10-12 12:27:21 +0530

    Update 2384. Largest Palindromic Number.java

--- Contest Problems/Weekly Contest 307/2384. Largest Palindromic Number.java
+++ Contest Problems/Weekly Contest 307/2384. Largest Palindromic Number.java
@@ -1,1 +1,30 @@
-
+class Solution {
+    public String largestPalindromic(String num) {
+        int[] fre = new int[127];
+        for(int i=0;i<num.length();i++){
+            fre[num.charAt(i)]++;
+        }
+        String left="";
+        StringBuilder right= new StringBuilder();
+        int c=0;
+        for(int i=0;i<num.length();i++){
+            for(char j='9';j>='0';j--){
+                c=c+1;
+                if(fre[j]>1 && (j>'0' || left.length()>0)){
+                    left+=j;
+                    right.append(j);
+                    fre[j]-=2;
+                    break;
+                }
+            }
+        }
+        for(char i='9';i>='0';i--){
+            if(fre[i]>0){
+                left+=i;
+                break;
+            }
+        }
+        right.reverse();
+        return left+(right.toString());
+    }
+}
