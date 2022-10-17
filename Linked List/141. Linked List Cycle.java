e7754755a1245da7ff55e0b53fce84b89bb99bfd
keerthana16-p <108286359+keerthana16-p@users.noreply.github.com>
2022-10-17 19:58:08 +0530

    Create 141. Linked List Cycle.java

---
+++ Linked List/141. Linked List Cycle.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slowptr = head;
+        ListNode fastptr = head;
+        while(slowptr!=null && fastptr!=null && fastptr.next!=null){
+            fastptr=fastptr.next.next;
+            slowptr=slowptr.next;
+            if(fastptr==slowptr){
+                return true;
+            }
+        }
+        return false;
+    }
+}
