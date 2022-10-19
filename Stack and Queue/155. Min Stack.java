6f260e2852fa0f56b51d719adf323315da6e7dcc
keerthana16-p <108286359+keerthana16-p@users.noreply.github.com>
2022-10-19 11:00:21 +0530

    Create 155. Min Stack.java

---
+++ Stack and Queue/155. Min Stack.java
@@ -0,0 +1,25 @@
+class MinStack {
+    Stack<Integer> stack = new Stack<Integer>();
+    int min = Integer.MAX_VALUE;
+    public void push(int val) {
+        if(val<=min){
+            stack.push(min);
+            min=val;
+        }
+        stack.push(val);
+    }
+    
+    public void pop() {
+       if(stack.pop()==min){
+           min=stack.pop();
+       } 
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return min;
+    }
+}
